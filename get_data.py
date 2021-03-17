948c7146f05c44e847121356794bd783d4431e13
peter <peter@precisionostech.com>
2021-03-17 12:09:36 -0700

    Downloaded the data

---
+++ get_data.py
@@ -0,0 +1,25 @@
+import requests
+from pathlib import Path
+
+urls = [
+    "https://github.com/CSSEGISandData/COVID-19/blob/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv",
+    "https://github.com/CSSEGISandData/COVID-19/blob/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv",
+    "https://github.com/CSSEGISandData/COVID-19/blob/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_recovered_global.csv",
+    "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_US.csv"
+    "https://github.com/CSSEGISandData/COVID-19/blob/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_US.csv",
+    "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/web-data/data/cases_country.csv"
+]
+
+data_dir = Path("data")
+
+if not data_dir.exists():
+    data_dir.mkdir()
+
+for url in urls:
+    name = url.split("/")[-1]
+    path = data_dir / name
+    r = requests.get(url)
+
+    with open(path, "w") as f:
+        f.write(str(r.content))
+    
