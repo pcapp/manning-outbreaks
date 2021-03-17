791fb8563cbd71aa09cf48ba5f8f882a246ec99d
peter <peter@precisionostech.com>
2021-03-17 13:41:37 -0700

    Examined the data from a high-level

--- get_data.py
+++ get_data.py
@@ -1,25 +1,21 @@
+import yaml
 import requests
 from pathlib import Path
-
-urls = [
-    "https://github.com/CSSEGISandData/COVID-19/blob/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv",
-    "https://github.com/CSSEGISandData/COVID-19/blob/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv",
-    "https://github.com/CSSEGISandData/COVID-19/blob/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_recovered_global.csv",
-    "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_US.csv",
-    "https://github.com/CSSEGISandData/COVID-19/blob/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_US.csv",
-    "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/web-data/data/cases_country.csv"
-]
 
 data_dir = Path("data")
 
 if not data_dir.exists():
     data_dir.mkdir()
 
-for url in urls:
-    name = url.split("/")[-1]
-    path = data_dir / name
+with open("data.yaml", "r") as f:
+    data = yaml.load(f, Loader=yaml.FullLoader)
+
+for entry in data:
+    name = entry['name']
+    url = entry['url']
+
     r = requests.get(url)
+    path = data_dir / f"{name}.csv"
 
     with open(path, "w") as f:
-        f.write(str(r.content))
-    
+        f.write(r.text)
